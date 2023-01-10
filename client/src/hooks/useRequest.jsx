5eab5d43ed0c4f4e910661a64378a2539feeb947
damian triebl <damiantriebl@gmail.com>
2023-01-10 16:21:37 -0300

    cambiar el uri

--- client/src/hooks/useRequest.jsx
+++ client/src/hooks/useRequest.jsx
@@ -8,7 +8,7 @@ const useRequest = ({ url, method, body, onSuccess, headers = '' }) => {
   const dispatch = useDispatch();
   const doSend = async (props = {}) => {
     setErrors(null);
-    const formatUrl = `http://localhost:4000${url}`;
+    const formatUrl = `https://backend-production-e99b.up.railway.app/${url}`;
     try {
       const response = await axios[method](formatUrl, { ...props, ...body })
       if (response) {
